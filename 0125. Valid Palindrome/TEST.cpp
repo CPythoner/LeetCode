743ba5251ec1cd572f32aa38d4aec9808d5b07a3
fatcat <1462261260@126.com>
2019-02-23 21:47:46 +0800

    0125. Valid Palindrome

---
+++ 0125. Valid Palindrome/TEST.cpp
@@ -0,0 +1,24 @@
+﻿// 0125. Valid Palindrome.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "0125. Valid Palindrome.h"
+
+#define CATCH_CONFIG_MAIN
+#include "../Catch2/single_include/catch.hpp"
+
+TEST_CASE("Valid Palindrome", "[isPalindrome]")
+{
+	Solution solution;
+
+	SECTION("1")
+	{
+		string s = "A man, a plan, a canal: Panama";
+		REQUIRE(solution.isPalindrome(s) == true);
+	}
+	SECTION("2")
+	{
+		string s = "race a car";
+		REQUIRE(solution.isPalindrome(s) == false);
+	}
+	system("pause");
+}
