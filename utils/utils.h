f96b6fc2850b0f93a7e8f470d960e75b0c0d7f71
wangqiang <wangq@yusur.tech>
2021-01-07 13:00:52 +0800

    改用模板方法

--- utils/utils.h
+++ utils/utils.h
@@ -3,11 +3,24 @@
 #include <vector>
 
 /**
- * @brief 比较两个 int 类型的 vector
+ * @brief 比较两个 vector 是否相等
  *
+ * @tparam T 支持 == 运算符进行比较的类型
  * @param vec1
  * @param vec2
  * @return true     两个 vector 相等
  * @return false    两个 vector 不相等
  */
-bool CompareIntVectors(std::vector<int> &vec1, std::vector<int> &vec2);
+template <typename T>
+bool CompareVectors(std::vector<T> const &vec1, std::vector<T> const &vec2)
+{
+        if (vec1.size() != vec2.size())
+        return false;
+
+    for (int i = 0; i < vec1.size(); i++)
+    {
+        if (vec1[i] != vec2[i])
+            return false;
+    }
+    return true;
+}
