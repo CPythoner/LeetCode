00db9cd4d460585c66e7376bbe07ad971a726df8
wangqiang <qiang0310@126.com>
2024-03-22 10:25:11 +0800

    add solution 120

---
+++ 0120.Triangle/TEST.cpp
@@ -0,0 +1,10 @@
+#include "0120.Triangle.h"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Triangle", "[triangle]")
+{
+    // Solution solution;
+
+    // Test case 1: Common case
+    REQUIRE(1 + 1 == 2);
+}
