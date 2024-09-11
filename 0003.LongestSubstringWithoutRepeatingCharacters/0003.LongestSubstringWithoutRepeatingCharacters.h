244bb15d8f09108be79c1685dc93b1103fbcf5f1
wangqiang <qiang0310@126.com>
2024-09-11 15:16:30 +0800

    添加新的C++解决方案和测试用例：Product of Array Except Self
    实现了一种新的解决方案来解决“Product of Array Except Self”问题，并为该解决方案添加了全面的测试用例。该解决方案利用前缀和后缀乘积的计算，避免使用除法运算符，并满足O(n)时间复杂度要求。此外，还提供了一个空间复杂度为O(1)的优化版本，该版本在不使用额外空间的情况下实现了相同的功能。

--- 0003.LongestSubstringWithoutRepeatingCharacters/0003.LongestSubstringWithoutRepeatingCharacters.h
+++ 0003.LongestSubstringWithoutRepeatingCharacters/0003.LongestSubstringWithoutRepeatingCharacters.h
@@ -1,6 +1,6 @@
 #pragma once
 
-/* 0003 无重复字符的最长子串
+/*
 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 
 示例 1:
@@ -22,6 +22,10 @@
 示例 4:
     输入: s = ""
     输出: 0
+
+提示：
+    0 <= s.length <= 5 * 10^4
+    s 由英文字母、数字、符号和空格组成
 */
 #include <string>
 #include <vector>
@@ -31,23 +35,34 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // 如果字符串为空，则直接返回0
         if (s.length() == 0)
             return 0;
 
+        // 初始化字符位置数组，ASCII码大小为128
         vector<int> pos(128, 0);
+        // 初始化子串起始位置i为0
         int i = 0;
+        // 初始化最大长度为0
         int max_len = 0;
 
+        // 遍历字符串中的每个字符
         for (int idx = 0; idx < s.size(); idx++)
         {
+            // 获取当前字符
             char tmp = s[idx];
-            if (pos[tmp] != 0)    // 表示该字符出现过
-                i = max(i, pos[tmp]);
+            // 如果当前字符之前出现过，并且上次出现的位置在当前子串起始位置i之后
+            if (pos[tmp] != 0 && pos[tmp] > i)
+                // 更新子串起始位置为当前字符上次出现位置的下一个位置
+                i = pos[tmp];
 
+            // 更新当前字符的位置为当前位置的下一个位置
             pos[tmp] = idx + 1;
+            // 更新最大长度
             max_len = max(max_len, idx - i + 1);
         }
 
+        // 返回最大长度
         return max_len;
     }
 };
